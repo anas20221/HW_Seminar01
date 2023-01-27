7287a9d1a7e6059c0097f4d72254c69ec7d8745e
AnaS <anastudy@yandex.ru>
2023-01-27 20:13:17 +0300

    number2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* задача 2
+Напишите программу, которая на вход принимает два числа и выдаёт, 
+какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7, min = 5
+a = 2 b = 10 -> max = 10, min = 2
+*/
